c39fcdd7bef6daa3175dd41ff9dc1b19eae2c8b9
m7md66 <mohamedal3alme6@gmail.com>
2024-01-29 13:11:50 +0200

    catalog api is done

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,46 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            //var dbUser = configuration.GetValue<string>("DatabaseSettings:username");
+            //var dbPass = configuration.GetValue<string>("DatabaseSettings:password ");
+            //MongoCredential credential = MongoCredential.CreateCredential(mechanism: "SCRAM-SHA-1", configuration.GetValue<string>("DatabaseSettings:DatabaseName"), "admin", "pass");
+
+//            var settings = new MongoClientSettings
+//            {
+//                Credential = credential,
+            
+//            };
+
+
+//            MongoCredential credential = MongoCredential.FromComponents(
+//    mechanism: "SCRAM-SHA-1",
+//    source: configuration.GetValue<string>("DatabaseSettings:DatabaseName"),
+//    username: "admin",
+//    password: "pass"
+//);
+
+
+//            var authenticatedClient = new MongoClient(settings);
+
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            //bool existProduct = Products.Find(p => true).Any();
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,22 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -14,6 +25,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
 }
 
 app.UseAuthorization();
